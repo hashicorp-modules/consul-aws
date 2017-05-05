9912edb8eb1a5889626dfbf6c723b54b2fa72c6b
Nicolas Corrarello <nicolas@corrarello.com>
2017-05-05 08:56:24 +0100

    Adding bucket for Backup

---
+++ bucket.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "consul_backup_bucket" {
+  bucket = "consul_backup_bucket"
+  acl    = "private"
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags {
+    Name        = "Consul Backup Bucket"
+  }
+}
--- policies.tf
+++ policies.tf
@@ -1,40 +1,43 @@
 data "aws_iam_policy_document" "assume_role" {
-	statement {
-		effect = "Allow"
-		actions = [
-			"sts:AssumeRole",
-		]
-		principals {
-			type = "Service"
-			identifiers = ["ec2.amazonaws.com"]
-		}
-	}
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 data "aws_iam_policy_document" "consul_server" {
-	statement {
-		sid = "AllowSelfAssembly"
-		effect = "Allow"
-		resources = ["*"]
-		actions = [
-			"autoscaling:DescribeAutoScalingGroups",
-			"autoscaling:DescribeAutoScalingInstances",
-			"ec2:DescribeAvailabilityZones",
-			"ec2:DescribeInstanceAttribute",
-			"ec2:DescribeInstanceStatus",
-			"ec2:DescribeInstances",
-			"ec2:DescribeTags",
-		]
-	}
-#	statement {
-#		sid = "AllowConsulBackups"
-#		effect = "Allow"
-#		resources = [
-#			"${var.backup_bucket_arn}",
-#			"${var.backup_bucket_arn}/*",
-#		]
-#		actions = [
-#			"s3:*"
-#		]
-#	}
+  statement {
+    sid = "AllowSelfAssembly"
+    effect = "Allow"
+    resources = ["*"]
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInstanceAttribute",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeInstances",
+      "ec2:DescribeTags",
+    ]
+  }
+  statement {
+    sid = "AllowConsulBackups"
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.consul_backup_bucket.arn}",
+      "${aws_s3_bucket.consul_backup_bucket.arn}/*",
+    ]
+    actions = [
+      "s3:*"
+    ]
+    lifecycle {
+      prevent_destroy = true
+    }
+  }
 }
